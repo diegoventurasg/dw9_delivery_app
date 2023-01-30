b369a5dcdc4ec1a01eb4e829d1cd68494e2c03c4
Diego Ventura <diegoventurasg@gmail.com>
2023-01-30 08:26:35 -0300

    initial setup

---
+++ lib/app/dw9_delivery_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Dw9DeliveryApp extends StatelessWidget {
+  const Dw9DeliveryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Delivery App',
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => Container(),
+      },
+    );
+  }
+}
