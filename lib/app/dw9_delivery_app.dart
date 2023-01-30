7eda6fbe23ce89d3604c9e67ba074a0446f95fd9
Diego Ventura <diegoventurasg@gmail.com>
2023-01-30 10:14:29 -0300

    create theme

--- lib/app/dw9_delivery_app.dart
+++ lib/app/dw9_delivery_app.dart
@@ -1,3 +1,4 @@
+import 'package:dw9_delivery_app/app/core/ui/theme/theme_config.dart';
 import 'package:flutter/material.dart';
 
 class Dw9DeliveryApp extends StatelessWidget {
@@ -8,6 +9,7 @@ class Dw9DeliveryApp extends StatelessWidget {
     return MaterialApp(
       title: 'Delivery App',
       debugShowCheckedModeBanner: false,
+      theme: ThemeConfig.theme,
       routes: {
         '/': (context) => Container(),
       },
